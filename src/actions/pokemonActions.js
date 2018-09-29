b59158d18ba18d474af168ee4f3931e359b8e67d
balazs <dbalazs40@gmail.com>
2018-09-29 10:08:04 +0200

    Created action creators

---
+++ src/actions/pokemonActions.js
@@ -0,0 +1,19 @@
+import {QUERY_POKEMON_ACTION} from './constants';
+
+export const QueryPokemonAction = (pokemons) => {
+	return {
+		type: QUERY_POKEMON_ACTION,
+		pokemons,
+	};
+};
+
+export const QueryPokemonActionAsync = () => {
+	return dispatch => {
+		fetch('https://pokeapi.co/api/v2/pokemon')
+			.then(response => response.json())
+			.then(response => {
+				console.log(response);
+			})
+			.catch(error => console.error(error));
+	};
+};
