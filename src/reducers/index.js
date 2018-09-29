e877b1d454cb9e01cfda926c7e7c6aa18f36d0ac
balazs <dbalazs40@gmail.com>
2018-09-29 10:00:07 +0200

    Added pokemon reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
+import pokemonReducer from './pokemonReducer';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+	pokemon: pokemonReducer,
+});
+
 const middlewares = [
 	thunk,
 ];
---
+++ src/reducers/pokemonReducer.js
@@ -0,0 +1,10 @@
+import {fromJS} from 'immutable';
+
+export default (state = fromJS({}), action) => {
+	switch (action.type) {
+
+		default: {
+			return state;
+		}
+	}
+};
