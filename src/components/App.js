ca677a2222175c3d441adeef5443fe750a304d0e
balazs <dbalazs40@gmail.com>
2018-09-29 13:53:27 +0200

    Added appbar

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,20 @@ import {Provider} from 'react-redux';
 import {store} from '../reducers/index';
 import PokemonListContainer from '../containers/PokemonListContainer';
 import DetailsModalContainer from '../containers/DetailsModalContainer';
+import AppBar from '@material-ui/core/AppBar/AppBar';
+import Toolbar from '@material-ui/core/Toolbar/Toolbar';
+import Typography from '@material-ui/core/Typography/Typography';
 
 export default () => (
 	<Provider store={store}>
 		<div>
+			<AppBar position="sticky">
+				<Toolbar>
+					<Typography variant="title" color="inherit">
+						Pokedex
+					</Typography>
+				</Toolbar>
+			</AppBar>
 			<PokemonListContainer/>
 			<DetailsModalContainer/>
 		</div>
